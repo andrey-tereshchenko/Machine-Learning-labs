a9494b5293d7dac23dbf5903070f345565aedb4f
Andrey <avtogol1998@gmail.com>
2019-09-24 13:18:58 +0300

    Completed task1

--- lab1/task1.py
+++ lab1/task1.py
@@ -6,28 +6,40 @@ df = pd.read_csv('data/1.csv')
 x, y = df['x'].values, df['y'].values
 
 
-def h(k, b):
-    # TODO
-    return np.zeros_like(x)
+def h(k, b, x):
+    return k * x + b
 
 
 def J(k, b):
-    # TODO
-    return 0
+    m = len(x)
+    param = (1 / (2 * m))
+    sum = 0
+    for i in range(len(x)):
+        sum += (h(k, b, x[i]) - y[i]) ** 2
+    return param * sum
 
 
 def grad(k, b):
-    # TODO
-    return 0, 0
+    m = len(y)
+    sum_1 = 0
+    sum_2 = 0
+    for i in range(m):
+        sum_2 += h(k, b, x[i]) - y[i]
+        sum_1 += (h(k, b, x[i]) - y[i]) * x[i]
+    grad_k = (1 / m) * sum_1
+    grad_b = (1 / m) * sum_2
+    return grad_k, grad_b
 
 
 k0, b0 = 0, 0
 learning_rate = 1e-1
 
 k, b = k0, b0
-for _ in range(100):
-    # Update k, b
-    pass
+for _ in range(500):
+    # print("k:" + str(k) + " b:" + str(b))
+    print("J:" + str(J(k, b)))
+    grad_k, grad_b = grad(k, b)
+    k, b = k - learning_rate * grad_k, b - learning_rate * grad_b
 
 plt.scatter(x, y, marker='.')
 plt.plot([0, 1], [b, k + b], c='red')
