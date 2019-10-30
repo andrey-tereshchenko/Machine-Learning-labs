315db2b2de2110e0ff804087413e7d535f755b59
Andrey <avtogol1998@gmail.com>
2019-10-30 13:17:26 +0200

    Update task2, added normalize

--- lab2/task2.py
+++ lab2/task2.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import bigfloat
 import pandas as pd
 
 df = pd.read_csv('data/2_train.csv')
@@ -11,12 +10,23 @@ def get_features(x1, x2):
     matrix = []
     for k in range(len(x1)):
         array = list()
-        for i in range(10):
-            for j in range(10):
+        for i in range(11):
+            for j in range(11):
                 if i + j <= 10:
                     array.append((x1[k] ** i) * (x2[k] ** j))
         matrix.append(np.array(array))
     return np.array(matrix)
+
+
+def normalize(x):
+    max_in_column = np.max(x, axis=0)
+    min_in_column = np.min(x, axis=0)
+    difference = max_in_column - min_in_column
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            if difference[j] != 0:
+                x[i][j] = (x[i][j] - min_in_column[j]) / difference[j]
+    return x
 
 
 def transform_y_for_one_vs_all(y, iter):
@@ -36,7 +46,7 @@ def accuracy(y_pred, y_true):
 def sigmoid(x):
     n = len(x)
     ones = np.array(n * [1])
-    return ones / (ones + np.array([float(bigfloat.exp(i)) for i in x]))
+    return ones / (ones + np.exp(x))
 
 
 def h(theta, x):
@@ -49,8 +59,6 @@ def cost_function(y, theta, x):
     y = np.transpose(y)
     b = np.transpose(h(theta, x))
     a = np.transpose(ones - h(theta, x))
-    a[a == 0] = 0.01
-    b[b == 0] = 0.01
     sum = np.matmul(-y, np.log(b)) - np.matmul((ones - y),
                                                np.log(a))
     return sum
@@ -64,7 +72,7 @@ def grad(theta, x, y):
 
 def gradient_decent(theta, x, y, alpha, iteration):
     for i in range(iteration):
-        print("iter" + str(i) + ': ' + str(cost_function(y, theta, x)))
+        # print("iter" + str(i) + ': ' + str(cost_function(y, theta, x)))
         theta = theta - alpha * grad(theta, x, y)
     return theta
 
@@ -81,39 +89,33 @@ def get_coordinate_for_plot(x1, x2, y):
 
 def one_vs_all(theta_0, theta_1, theta_2, x):
     h_matrix = np.array([h(theta_0, x), h(theta_1, x), h(theta_2, x)])
-    print(h_matrix.shape)
-    print(h_matrix)
     y_predict = np.argmax(h_matrix, axis=0)
     return y_predict
 
 
-
-
-
 x = get_features(x1_train, x2_train)
-theta = np.zeros(shape=(64, 1))
+x = normalize(x)
+theta = np.zeros(shape=(66, 1))
 y_0 = np.reshape(np.array(transform_y_for_one_vs_all(y_train, 0)), newshape=(500, 1))
 y_1 = np.reshape(np.array(transform_y_for_one_vs_all(y_train, 1)), newshape=(500, 1))
 y_2 = np.reshape(np.array(transform_y_for_one_vs_all(y_train, 2)), newshape=(500, 1))
-alpha = 1e-20
-iteration = 100
+alpha = 30
+iteration = 5000
 theta_0 = gradient_decent(theta.T[0], x, y_0.T[0], alpha, iteration)
 theta_1 = gradient_decent(theta.T[0], x, y_1.T[0], alpha, iteration)
-theta_2 = gradient_decent(theta.T[0], x, y_2.T[0], 1e-19, 291)
-
+theta_2 = gradient_decent(theta.T[0], x, y_2.T[0], alpha, iteration)
 
 df = pd.read_csv('data/2_test.csv')
 x1_test, x2_test, y_test = [df[k].values for k in ['x1', 'x2', 'y']]
 x_test = get_features(x1_test, x2_test)
+x_test = normalize(x_test)
+train_result = one_vs_all(theta_0, theta_1, theta_2, x)
 test_result = one_vs_all(theta_0, theta_1, theta_2, x_test)
 
-print(y_train)
-yy = h(theta_2, x)
-yy[yy > 0.5] = 1
-yy[yy <= 0.5] = 0
-x1_plot, x2_plot = get_coordinate_for_plot(x1_train, x2_train, yy)
-plt.figure(figsize=(6, 6))
-plt.scatter(x1_train, x2_train, c=y_train, marker='.')
-plt.plot(x1_plot, x2_plot, c='red')
-plt.show()
+# x1_plot, x2_plot = get_coordinate_for_plot(x1_train, x2_train, result)
+# plt.figure(figsize=(6, 6))
+# plt.scatter(x1_train, x2_train, c=y_train, marker='.')
+# plt.plot(x1_plot, x2_plot, c='red')
+# plt.show()
+print('Train accuracy: %.2f' % (accuracy(train_result, y_train) * 100) + '%')
 print('Final accuracy: %.2f' % (accuracy(test_result, y_test) * 100) + '%')
--- lab2/test.py
+++ lab2/test.py
@@ -3,5 +3,10 @@ import numpy as np
 matrix = np.array([[1, 2, 3, 5],
                    [2, 3, 4, 1],
                    [4, 1, 2, 2]])
-max = np.argmax(matrix, axis=0)
+
+min = np.min(matrix, axis=0)
+max = np.max(matrix, axis=0)
+difference = max - min
 print(max)
+print(min)
+print(difference)
